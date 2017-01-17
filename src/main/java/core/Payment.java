0fb9711f854bf52e2a25e165dc1dd006af30c768
mtnview101 <mtnview101@gmail.com>
2017-01-16 22:28:39 -0800

    3 commit

--- src/main/java/core/Payment.java
+++ src/main/java/core/Payment.java
@@ -17,39 +17,44 @@ public class Payment {
 
               Logger logger = Logger.getLogger("");
               logger.setLevel(Level.OFF);
+              
               WebDriver driver;
               final long start = System.currentTimeMillis();           
               if (browser.equalsIgnoreCase("chrome")) {System.setProperty("webdriver.chrome.driver","./src/main/resources/webdrivers/pc/chromedriver.exe");
-              System.setProperty("webdriver.chrome.silentOutput", "true");
-              ChromeOptions option = new ChromeOptions();
-              option.addArguments("-start-fullscreen");
-              driver = new ChromeDriver(option);
-              driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);}
+	              System.setProperty("webdriver.chrome.silentOutput", "true");
+	              ChromeOptions option = new ChromeOptions();
+	              option.addArguments("-start-fullscreen");
+              
+	              driver = new ChromeDriver(option);
+	              //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); What is a function of this method????
+	              }
               else{driver = new HtmlUnitDriver();((HtmlUnitDriver) driver).setJavascriptEnabled(true);}
+              
               driver.get(url);
-// "$1,654.55";
+
               String string_monthly_payment = driver.findElement(By.id("id_monthly_payment")).getText();
+              System.out.println("string_monthly_payment: "+string_monthly_payment);
               String regex = "^"
                            + "(?:\\$)?(?:\\s*)?((?:\\d{1,3})(?:\\,)?(?:\\d{3})?(?:\\.)?(\\d{0,2})?)"
                            + "$";
               Pattern p = Pattern.compile(regex);
               Matcher m = p.matcher(string_monthly_payment);
-              m.find();
-              // 1,654.55
-              double monthly_payment = Double.parseDouble(m.group(1).replaceAll(",", ""));
-              // 1654.55 * 12 = 19854.60
-             double annual_payment = new BigDecimal(monthly_payment * 12).setScale(2, RoundingMode.HALF_UP).doubleValue(); 
-              // 19854.6
-              DecimalFormat df = new DecimalFormat("0.00");
+              m.find(); // "$1,654.55"
+              
+              double monthly_payment = Double.parseDouble(m.group(1).replaceAll(",", "")); // 1,654.55
+              double annual_payment = new BigDecimal(monthly_payment * 12).setScale(2, RoundingMode.HALF_UP).doubleValue(); // 1654.55 * 12 = 19854.6
+              DecimalFormat df = new DecimalFormat("0.00");// 19854.60
               String f_annual_payment = df.format(annual_payment);
-             driver.findElement(By.id("id_annual_payment")).sendKeys(String.valueOf(f_annual_payment));
+              
+              driver.findElement(By.id("id_annual_payment")).sendKeys(String.valueOf(f_annual_payment));
               driver.findElement(By.id("id_validate_button")).submit();
+              
               String actual_result = driver.findElement(By.id("id_result")).getText();
               final long finish = System.currentTimeMillis();
               System.out.println("String: \t" + m.group(0)); // capturing whole thing
               System.out.println("Annual Payment: " + f_annual_payment);
               System.out.println("Result: \t" + actual_result);
-              System.out.println("Resonse time: \t" + (finish - start) + " milliseconds:");
-              Thread.sleep(2000);
+              System.out.println("Resonse time: \t" + (finish - start) + " milliseconds");
+              Thread.sleep(5000);
               driver.quit(); }}
 
